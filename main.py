ab318ef4605011638d236c81ba49e1a13e20551d
Erica Shackelford <ericalshackelford@gmail.com>
2024-05-17 18:03:37 +0000

    Create Vehicle class with max_speed and mileage attributes

---
+++ main.py
@@ -0,0 +1,8 @@
+# Creating a Vehicle class with attributes for maxium speed and mileage
+
+class Vehicle:
+
+# Constructor method to initialize the attributes  
+  def __init__(self, max_speed, mileage):
+    self.max_speed = max_speed
+    self.mileage = mileage
